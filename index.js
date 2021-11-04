6abb07ea720dc9edff06f4878596f42ea3dd8475
Jimmy <himhimjimmy@yahoo.com.hk>
2021-11-04 16:25:41 +0000

    revamped buttons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const textureSize = 16; // side length of each tile in pixels
 const worldSize = 500; // worldSize^2 tiles
 const viewportX = 0;
 const viewportY = 0;
+const renderRadius = 10;
 
 var player;
 var cam;
@@ -15,6 +16,7 @@ var mousePos;
 var buttons = [];
 var toggles = [];
 var entities = [];
+var rendered = [];
 
 var playing = false;
 var mainMenu = true;
@@ -24,6 +26,8 @@ var selection = false;
 var debug = true;
 var fullsrn = false;
 var entityShadow = true;
+
+var first = true;
 
 var tiles = [];
 var tileIds = [];
@@ -40,13 +44,36 @@ function generate() {
         id = 0;
       }
       tileIds.push(id);
-      world.image(tiles[tileIds[i + j * worldSize]], i * textureSize, (worldSize - j - 1) * textureSize);
+
     }
   }
 }
 
 function worldUpdate() {
-  viewport.translate(-cam.pos.x, cam.pos.y);
+  viewport.translate(-cam.pos.x * textureSize, cam.pos.y * textureSize);
+
+  if (keyIsDown(87) || keyIsDown(65) || keyIsDown(83) || keyIsDown(68) || first) {
+    for (let j = floor(cam.pos.y) - renderRadius; j < floor(cam.pos.y) + renderRadius * 2; j++) {
+      for (let i = floor(cam.pos.x) - renderRadius; i < floor(cam.pos.x) + renderRadius * 2; i++) {
+        if (dist(cam.pos.x, cam.pos.y, i, j) < renderRadius && i + j * worldSize >= 0 && i + j * worldSize < tileIds.length && rendered[i + j * worldSize] != tileIds[i + j * worldSize]) {
+          // if (first == true) {
+          world.image(tiles[tileIds[i + j * worldSize]], i * textureSize, (worldSize - j - 1) * textureSize);
+          // } else {
+          //   world.image(tiles[1], i * textureSize, (worldSize - j - 1) * textureSize);
+          // }
+          // console.log(first);
+          rendered[i + j * worldSize] = tileIds[i + j * worldSize];
+        }
+      }
+    }
+    first = false;
+  }
+
+  entities.sort((a, b) => { return - a.pos.y + b.pos.y });
+  entities.forEach(entity => {
+    entity.update();
+  });
+
   viewport.image(world, 0, -worldSize * textureSize);
 }
 
@@ -96,12 +123,50 @@ function setup() {
   viewport.textFont(font);
   separateTextures(3, 2); // number of tiles (x, y) in texture
   noCursor();
-  cursor(CROSS);
+  cursor('crosshair');
+
+  // main menu
+  // buttons
+  buttons[0] = new Button('Play', viewport.width / 2, viewport.height * 0.60, true, 11, null, null, 100, 35, 10, 10, 10, 10);
+  buttons[1] = new Button('Settings', viewport.width / 2, viewport.height * 0.60 + 40, true, 11, null, null, 100, 35, 10, 10, 10, 10);
+
+  //settings
+  // toggles
+  toggles[0] = new Toggle('Fullscreen', fullsrn, 50, 100);
+  toggles[1] = new Toggle('Debug', debug, 50, 100 + 30);
+  toggles[2] = new Toggle('Entity shadow', entityShadow, 50, 100 + 60);
+  // buttons
+  buttons[6] = new Button('X', width - 20, 20, 40, 40);
+
+  // selection menu
+  // buttons
+  // buttons[2] = new Button('', width * 0.25, 110, 150, 40);
+  // buttons[3] = new Button('', width * 0.25, 160, 150, 40);
+  // buttons[4] = new Button('', width * 0.25, 210, 150, 40);
+  // buttons[5] = new Button('', width * 0.25, 260, 150, 40);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   viewport = createGraphics(windowWidth, windowHeight);
+
+  // mainMenu
+    buttons[0] = new Button('Play', viewport.width / 2, viewport.height * 0.60, true, 11, null, null, 100, 35, 10, 10, 10, 10);
+    buttons[1] = new Button('Settings', viewport.width / 2, viewport.height * 0.60 + 40, true, 11, null, null, 100, 35, 10, 10, 10, 10);
+
+  // settingsMenu
+    toggles[0] = new Toggle('Fullscreen', fullsrn, 50, 100);
+    toggles[1] = new Toggle('Debug', debug, 50, 100 + 30);
+    toggles[2] = new Toggle('Entity shadow', entityShadow, 50, 100 + 60);
+
+    buttons[6] = new Button('X', viewport.width - 40, 20, true, 20);
+
+  // if (selection) {
+  //   buttons[2] = new Button('', width * 0.25, 110, 150, 40);
+  //   buttons[3] = new Button('', width * 0.25, 160, 150, 40);
+  //   buttons[4] = new Button('', width * 0.25, 210, 150, 40);
+  //   buttons[5] = new Button('', width * 0.25, 260, 150, 40);
+  // }
 }
 
 function draw() {
@@ -135,9 +200,6 @@ function draw() {
     viewport.textSize(11);
     viewport.text('Note: this is not a virus, just a game!', viewport.width - 100, viewport.height - 20)
 
-    // buttons
-    buttons[0] = new Button('Play', viewport.width / 2, viewport.height * 0.60, 100, 35);
-    buttons[1] = new Button('Settings', viewport.width / 2, viewport.height * 0.60 + 40, 100, 35);
     for (let i = 0; i <= 1; i++) {
       buttons[i].show();
     }
@@ -152,89 +214,68 @@ function draw() {
     viewport.fill(59, 52, 59);
     viewport.noStroke();
     viewport.rect(0, 0, viewport.width, viewport.height);
-    // viewport.background(59, 52, 59);
 
     // title
     viewport.fill(255);
     viewport.textSize(32);
     viewport.text('Settings', 100, 30);
 
-    // toggles
-    toggles[0] = new Toggle('Fullscreen', fullsrn, 50, 100);
-    toggles[1] = new Toggle('Debug', debug, 50, 100 + 30);
-    toggles[2] = new Toggle('Entity shadow', entityShadow, 50, 100 + 60);
     for (let i = 0; i <= 2; i++) {
       toggles[i].show();
     }
+    buttons[6].show();
     viewport.pop();
   }
 
   if (playing) {
     fps = 1 / (deltaTime / 1000);
-    // viewport.image(backdrop, 0, 0);
+    mousePos = { x: (mouseX - viewport.width / 2) / (textureSize * cam.scl) + cam.pos.x, y: -(mouseY - viewport.height / 2) / (textureSize * cam.scl) + cam.pos.y };
+
+    // background
     viewport.fill('#93bdc2');
     viewport.noStroke();
     viewport.rect(0, 0, viewport.width, viewport.height);
 
-    mousePos = createVector((mouseX - viewport.width / 2 + cam.pos.x * cam.scl) / (textureSize * cam.scl), -(mouseY - viewport.height / 2 - cam.pos.y * cam.scl) / (textureSize * cam.scl));
-
     viewport.push();
     cam.update();
     worldUpdate();
-
-    entities.sort((a, b) => { return - a.pos.y + b.pos.y });
-    entities.forEach(entity => {
-      entity.update();
-    });
     viewport.pop();
 
     if (debug) {
       viewport.push();
       viewport.fill(0, 100);
-      viewport.rect(0, 0, 280, 110);
+      viewport.rect(0, 0, 400, (15+1)*8 + 20);
 
       viewport.textAlign(LEFT, TOP);
       viewport.noStroke();
       viewport.fill(255);
       viewport.textSize(15);
 
-      viewport.text('debug', 10, 5);
-      viewport.text('scale: ' + floor(cam.scl), 10, 20);
-      viewport.text('player position: ' + floor(player.pos.x / textureSize) + ', ' + floor(player.pos.y / textureSize), 10, 35);
-      // viewport.text('fps: ' + floor(fps), 10, 30);
-      viewport.text('mouse position: ' + floor(mousePos.x) + ', ' + floor(mousePos.y), 10, 50);
-      viewport.text('camera position: ' + floor(cam.pos.x / textureSize) + ', ' + floor(cam.pos.y / textureSize), 10, 65);
-      viewport.text('currently on: ' + tileNames[tileIds[floor(player.pos.x / textureSize) + floor(player.pos.y / textureSize) * worldSize]], 10, 80);
-
+      viewport.text('debug' + '\n' + 'scale: ' + floor(cam.scl) + '\n' + 'player position: ' + floor(player.pos.x) + ', ' + floor(player.pos.y) + '\n' + 'mouse position: ' + floor(mousePos.x) + ', ' + floor(mousePos.y) + '\n' + 'camera position: ' + floor(cam.pos.x) + ', ' + floor(cam.pos.y) + '\n' + 'currently on: ' + tileNames[tileIds[floor(player.pos.x) + floor(player.pos.y) * worldSize]] + '\n' + 'entity count: ' + entities.length, 10, 5)
       viewport.pop();
     }
   }
 
   if (selection) {
-    push();
+    viewport.push();
     // background
-    fill(59, 52, 59);
-    noStroke();
-    rect(0, 0, windowWidth, windowHeight);
+    viewport.fill(59, 52, 59);
+    viewport.noStroke();
+    viewport.rect(0, 0, windowWidth, windowHeight);
 
     // title
-    fill(255);
-    textSize(32);
-    text('Select', 100, 30);
+    viewport.fill(255);
+    viewport.textSize(32);
+    viewport.text('Select', 100, 30);
 
-    fill(49, 42, 49);
-    noStroke();
-    rect(width * 0.25 + 100, 90, width * 0.45, height * 0.7, 10, 10, 10, 10);
+    viewport.fill(49, 42, 49);
+    viewport.noStroke();
+    viewport.rect(width * 0.25 + 100, 90, width * 0.45, height * 0.7, 10, 10, 10, 10);
 
-    // buttons
-    buttons[2] = new Button('', width * 0.25, 110, 150, 40);
-    buttons[3] = new Button('', width * 0.25, 160, 150, 40);
-    buttons[4] = new Button('', width * 0.25, 210, 150, 40);
-    buttons[5] = new Button('', width * 0.25, 260, 150, 40);
     for (let i = 2; i <= 5; i++) {
       buttons[i].show();
     }
-    pop();
+    viewport.pop();
   }
 
   image(viewport, viewportX, viewportY);
@@ -249,11 +290,14 @@ function mousePressed() {
       mainMenu = false;
       selection = false;
 
+      first = true;
+
       tileIds = [];
       entities = [];
+      rendered = [];
 
       cam = new Camera();
-      player = new Entity(2, 500, 500, 1, 3, 'player');
+      player = new Entity(2, 0, 0, 1, 1, 1, 'player');
       entities.push(player);
       world = createGraphics(worldSize * textureSize, worldSize * textureSize);
       generate();
@@ -288,14 +332,14 @@ function mousePressed() {
     // }
   }
 
-  if (playing && mousePos != null) {
-    entities.push(new Entity(2, mousePos.x, mousePos.y, 1, 1, 'npc'));
+  if (playing) {
+    new Entity(2, mousePos.x, mousePos.y, 1, 1, 0, 'npc');
   }
 }
 
 class Camera {
   constructor() {
-    this.pos = createVector(0 * textureSize, 0 * textureSize);
+    this.pos = { x: 0 * textureSize, y: 0 * textureSize };
     this.scl = 2.5;
   }
 
@@ -333,35 +377,58 @@ class Camera {
 }
 
 class Button {
-  constructor(name, x, y, w, h) {
+  constructor(name, x, y, hasHoverEffect, textSize, textColor, buttonColor, width, height, topLeft, topRight, bottomRight, bottomLeft) {
     this.name = name;
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
+    this.pos = { x: x, y: y };
+    this.size = { w: width, h: height, ts: textSize };
+    this.cornerRadiuses = { tl: topLeft, tr: topRight, br: bottomRight, bl: bottomLeft };
+    this.col = { tc: textColor, bc: buttonColor };
+    this.bool = hasHoverEffect;
+
+    if (name == null) {
+      this.name = '';
+    }
+    if (hasHoverEffect == null) {
+      this.bool = false;
+    }
+    if (textSize == null) {
+      this.size.ts = 11;
+    }
+    if (textColor == null) {
+      this.col.tc = 0;
+    }
+    if (buttonColor == null) {
+      this.col.bc = 255;
+    }
+    if (width == null) {
+      this.size.w = textWidth(this.name) + 10;
+    }
+    if (height == null) {
+      this.size.h = 20;
+    }
   }
 
   show() {
     viewport.push();
     viewport.noStroke();
-    viewport.fill(255);
-    viewport.rect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h, 10, 10, 10, 10);
-    viewport.fill(0);
-    viewport.textSize(11);
-    viewport.text(this.name, this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
+    viewport.fill(this.col.bc);
+    viewport.rect(this.pos.x - this.size.w / 2, this.pos.y - this.size.h / 2, this.size.w, this.size.h, this.cornerRadiuses.tl, this.cornerRadiuses.tr, this.cornerRadiuses.br, this.cornerRadiuses.bl);
+    viewport.fill(this.col.tc);
+    viewport.textSize(this.size.ts);
+    viewport.text(this.name, this.pos.x - this.size.w / 2, this.pos.y - this.size.h / 2, this.size.w, this.size.h);
 
-    if (this.hover()) {
+    if (this.hover() && this.bool) {
       viewport.fill(255);
       viewport.strokeWeight(10);
       viewport.textSize(20);
-      viewport.text('>', this.x - this.w / 2 - 15, this.y - 4);
-      viewport.text('<', this.x + this.w / 2 + 15, this.y - 4);
+      viewport.text('>', this.pos.x - this.size.w / 2 - 15, this.pos.y - 4);
+      viewport.text('<', this.pos.x + this.size.w / 2 + 15, this.pos.y - 4);
     }
     viewport.pop();
   }
 
   hover() {
-    if (mouseX <= viewportX + this.x + this.w / 2 && mouseX >= viewportX + this.x - this.w / 2 && mouseY <= viewportY + this.y + this.h / 2 && mouseY >= viewportY + this.y - this.h / 2) {
+    if (mouseX <= viewportX + this.pos.x + this.size.w / 2 && mouseX >= viewportX + this.pos.x - this.size.w / 2 && mouseY <= viewportY + this.pos.y + this.size.h / 2 && mouseY >= viewportY + this.pos.y - this.size.h / 2) {
       return true;
     }
     return false;
@@ -372,8 +439,7 @@ class Toggle {
   constructor(name, state, x, y) {
     this.name = name;
     this.state = state;
-    this.x = x;
-    this.y = y;
+    this.pos = { x: x, y: y };
   }
 
   show() {
@@ -382,8 +448,8 @@ class Toggle {
     viewport.textSize(20);
     viewport.fill(255);
     viewport.noStroke();
-    viewport.text(this.name, this.x, this.y);
-    viewport.text(this.state, viewport.width - 80, this.y);
+    viewport.text(this.name, this.pos.x, this.pos.y);
+    viewport.text(this.state, viewport.width - 80, this.pos.y);
 
     viewport.stroke(255);
     viewport.strokeWeight(2.5);
@@ -392,12 +458,12 @@ class Toggle {
     } else {
       viewport.fill(59, 52, 59);
     }
-    viewport.rect(viewport.width - 120, this.y, 20, 10, 10, 10, 10, 10);
+    viewport.rect(viewport.width - 120, this.pos.y - 5, 20, 10, 10, 10, 10, 10);
     viewport.pop();
   }
 
   hover() {
-    if (mouseX <= viewportX + viewport.width - 120 + 20 + 2.5 && mouseX >= viewportX + viewport.width - 120 - 2.5 && mouseY <= viewportY + this.y + 10 + 2.5 && mouseY >= viewportY + this.y - 2.5) {
+    if (mouseX <= viewportX + viewport.width - 120 + 20 + 2.5 && mouseX >= viewportX + viewport.width - 120 - 2.5 && mouseY <= viewportY + this.pos.y + 10 + 2.5 && mouseY >= viewportY + this.pos.y - 2.5) {
       return true;
     }
     return false;
@@ -405,12 +471,23 @@ class Toggle {
 }
 
 class Entity {
-  constructor(id, x, y, size, speed, type) {
-    this.id = id;
-    this.pos = createVector(x * textureSize, y * textureSize);
-    this.size = size * textureSize;
-    this.speed = speed;
-    this.type = type;
+  constructor(tileId, x, y, width, height, speed, type) {
+    if (tileId > tiles.length || width <= 0 || height <= 0 || speed < 0 || type == null) {
+      this.id = 2;
+      this.pos = { x: player.pos.x, y: player.pos.y };
+      this.size = { w: 1, h: 1 };
+      this.speed = 1;
+      this.type = 'npc';
+    } else {
+      this.id = tileId;
+      this.pos = { x: x, y: y };
+      this.size = { w: width, h: height };
+      this.speed = speed;
+      this.type = type;
+    }
+
+    this.mesh = createGraphics(this.size.w * textureSize, this.size.h * textureSize);
+    entities.push(this);
   }
 
   update() {
@@ -438,36 +515,53 @@ class Entity {
   }
 
   show() {
-    viewport.push();
-    if (this.pos.x < this.size / 2) {
-      this.pos.x = this.size / 2;
+    world.push();
+    if (this.pos.x < this.size.w / 2) {
+      this.pos.x = this.size.w / 2;
     }
-    if (this.pos.y < this.size / 2) {
-      this.pos.y = this.size / 2;
+    if (this.pos.y < this.size.h / 2) {
+      this.pos.y = this.size.h / 2;
     }
-    if (this.pos.x > this.size * worldSize - this.size / 2) {
-      this.pos.x = this.size * worldSize - this.size / 2;
+    if (this.pos.x > this.size.w * worldSize - this.size.w / 2) {
+      this.pos.x = this.size.w * worldSize - this.size.w / 2;
     }
-    if (this.pos.y > this.size * worldSize - this.size / 2) {
-      this.pos.y = this.size * worldSize - this.size / 2;
+    if (this.pos.y > this.size.h * worldSize - this.size.h / 2) {
+      this.pos.y = this.size.h * worldSize - this.size.h / 2;
     }
 
-    viewport.translate(this.pos.x, -this.pos.y);
-
-    if (dist(cam.pos.x, cam.pos.y, this.pos.x, this.pos.y) < 10 * textureSize) {
+    if (dist(cam.pos.x, cam.pos.y, this.pos.x, this.pos.y) < renderRadius) {
       // shadow
       if (entityShadow) {
-        viewport.noStroke();
-        viewport.fill(0, 50);
-        viewport.ellipse(0, 0, (this.size / 2 + 5), this.size / 4);
+        if (this.type == 'player' || this.type == 'npc') {
+          this.mesh.noStroke();
+          this.mesh.fill(0, 50);
+          this.mesh.ellipse(this.size.w * textureSize / 2, this.size.h * textureSize, (this.size.w * textureSize / 2 + 5), this.size.h * textureSize / 4);
+        }
       }
 
-      viewport.image(tiles[this.id], - this.size / 2, - this.size, this.size);
+      this.mesh.image(tiles[this.id], 0, 0);
     }
-    viewport.pop();
+
+    if (this.mesh != null) {
+
+      world.translate(this.pos.x, this.pos.y);
+      world.image(this.mesh, - this.size.w / 2, - this.size.h);
+
+    }
+    world.pop();
   }
 }
 
 function dist(x1, y1, x2, y2) {
   return ((x1 - x2) ^ 2 + (y1 - y2) ^ 2) ^ 0.5;
+}
+
+class Slider {
+  constructor(name, x, y, start, end) {
+    this.name = name;
+    this.pos = { x: x, y: y };
+    this.s = start;
+    this.e = end;
+
+  }
 }
