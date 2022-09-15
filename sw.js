a998cb6f65cd61f8e5e819d43f9851a130c0b8c2
cchjimmy <92587458+cchjimmy@users.noreply.github.com>
2022-09-15 21:43:21 +0100

    added more static files to cache list

--- sw.js
+++ sw.js
@@ -1,22 +1,24 @@
 // credit: https://www.youtube.com/watch?v=dXuvT4oollQ
-
 const cacheName = "cach-v1";
 const resourcesToPrecache = [
   '/',
   '/index.html',
-  '/style.css',
+  '/res/icons/android-chrome-192x192.png',
+  '/res/icons/android-chrome-512x512.png',
+  '/res/icons/favicon-16x16.png',
+  '/res/icons/favicon-32x32.png',
+  '/site.webmanifest',
   '/src/main.js',
   '/src/menus.js',
+  '/src/utilities/Canvas2D.js',
+  '/src/utilities/ECS.js',
   '/src/utilities/Engine.js',
+  '/src/utilities/debounce.js',
   '/src/utilities/gui/GuiMaker.js',
   '/src/utilities/gui/guiStyle.css',
-  '/src/utilities/Canvas2D.js',
-  '/src/utilities/debounce.js',
-  '/src/utilities/ECS.js',
   '/src/utilities/randomRange.js',
   '/src/utilities/uuidv4.js',
-  '/res/icons/android-chrome-192x192.png',
-  '/res/icons/favicon-32x32.png',
+  '/style.css',
 ]
 
 self.addEventListener("install", e => {
@@ -32,8 +34,16 @@ self.onactivate = (e) => {
   e.waitUntil(clients.claim());
 }
 
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(response => {
-    return response || fetch(e.request);
-  }));
-});
+
+// credit: https://medium.com/swlh/how-to-make-your-web-apps-work-offline-be6f27dd28e
+self.onfetch = (e) => {
+  e.respondWith(
+    caches.match(e.request).then(
+      response => response
+    ).catch(fetch(e.request).then(response => {
+      caches.open(cacheName).then(
+        cache => cache.put(e.request, response)
+      )
+    }).catch((err) => { console.log(err); }))
+  )
+}
